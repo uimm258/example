c3d8a0a14b6375391d3ecdc69987e1af893e62a6
Hongni Liu <uimm258@gmail.com>
2021-09-17 10:37:28 -0700

    ...

--- MovieTheater/server/database.sql
+++ MovieTheater/server/database.sql
@@ -1,4 +1,3 @@
--- CREATE DATABASE "saga_movies_weekend"
 CREATE TABLE "users" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) NOT NULL,
@@ -17,9 +16,6 @@ CREATE TABLE "genres" (
   "name" VARCHAR(80) NOT NULL
 );
 
--- JUNCTION TABLE
--- Movies can have multiple genres and each genre can be applied to multiple movies
--- This is many-to-many!
 CREATE TABLE "movies_genres" (
   "id" SERIAL PRIMARY KEY,
   "movie_id" INT  NOT NULL,
@@ -47,9 +43,8 @@ CREATE TABLE "users_genres" (
         REFERENCES users(id)
 );
 
---------[ DATA! ]---------
+-----------------
 
--- starter movies
 INSERT INTO "movies" ("title", "poster", "description")
 VALUES 
 ('Avatar', 'images/avatar.jpeg', 'Avatar (marketed as James Cameron''s Avatar) is a 2009 American epic science fiction film directed, written, produced, and co-edited by James Cameron, and stars Sam Worthington, Zoe Saldana, Stephen Lang, Michelle Rodriguez, and Sigourney Weaver. The film is set in the mid-22nd century, when humans are colonizing Pandora, a lush habitable moon of a gas giant in the Alpha Centauri star system, in order to mine the mineral unobtanium, a room-temperature superconductor. The expansion of the mining colony threatens the continued existence of a local tribe of Na''vi – a humanoid species indigenous to Pandora. The film''s title refers to a genetically engineered Na''vi body operated from the brain of a remotely located human that is used to interact with the natives of Pandora.'),
@@ -67,7 +62,6 @@ VALUES
 ('Titanic', 'images/titanic.jpg', 'Titanic is a 1997 American epic romance and disaster film directed, written, co-produced, and co-edited by James Cameron. A fictionalized account of the sinking of the RMS Titanic, it stars Leonardo DiCaprio and Kate Winslet as members of different social classes who fall in love aboard the ship during its ill-fated maiden voyage.'),
 ('Toy Story', 'images/toy-story.jpg', 'Toy Story is a 1995 American computer-animated adventure comedy film produced by Pixar Animation Studios and released by Walt Disney Pictures. The feature-film directorial debut of John Lasseter, it was the first feature-length film to be entirely computer-animated, as well as the first feature film from Pixar. The screenplay was written by Joss Whedon, Andrew Stanton, Joel Cohen, and Alec Sokolow from a story by Lasseter, Stanton, Pete Docter, and Joe Ranft. The film features music by Randy Newman, and was executive-produced by Steve Jobs and Edwin Catmull. The film features the voices of Tom Hanks, Tim Allen, Don Rickles, Wallace Shawn, John Ratzenberger, Jim Varney, Annie Potts, R. Lee Ermey, John Morris, Laurie Metcalf, and Erik von Detten. Taking place in a world where anthropomorphic toys come to life when humans are not present, its plot focuses on the relationship between an old-fashioned pull-string cowboy doll named Woody and an astronaut action figure, Buzz Lightyear, as they evolve from rivals competing for the affections of their owner Andy Davis to friends who work together to be reunited with him after being separated.');
 
--- starter genres
 INSERT INTO "genres" ("name")
 VALUES 
 ('Adventure'),
@@ -79,32 +73,31 @@ VALUES
 ('Epic'),
 ('Fantasy'),
 ('Musical'),
-('Romantic'),         --10
-('Science Fiction'),  --11
-('Space-Opera'),      --12
-('Superhero');        --13
+('Romantic'),         
+('Science Fiction'),  
+('Space-Opera'),      
+('Superhero');        
 
 INSERT  INTO "users"("username", "password")
 VALUES
 ('maymay', 'Password1');
 
--- starter movies and genres data
 INSERT INTO "movies_genres" ("movie_id", "genre_id")
 VALUES 
-(1,1), (1,3), (1,4),      -- Avatar
-(2,1), (2,11), (2,12),    -- Beauty
-(3,3),                    -- Cpt Marvel
-(4,4), (4,7),             -- Nemo
-(5,3),                    -- Gone Girl
-(6,12),                   -- Potter
-(7,9),(7,2),              -- Bond
-(8,4),                    -- Pi
-(9,4),                    -- Monsters
-(10,4),                   -- Star Wars
-(11,6), (11,11),          -- Martian
-(12,8), (12,9),           -- Social Net
-(13,4), (13,10), (13,6),  -- Titanic
-(14,3), (14,2), (14,4);   -- Toy Story
+(1,1), (1,3), (1,4),     
+(2,1), (2,11), (2,12),    
+(3,3),                    
+(4,4), (4,7),             
+(5,3),                    
+(6,12),                   
+(7,9),(7,2),              
+(8,4),                    
+(9,4),                    
+(10,4),                   
+(11,6), (11,11),          
+(12,8), (12,9),           
+(13,4), (13,10), (13,6),  
+(14,3), (14,2), (14,4);   
 
 INSERT INTO "users_genres" ("user_id", "genre_id")
 VALUES
